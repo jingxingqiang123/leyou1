4455923b1c73ff05c62b3abbc979d404efa072ef
jingxingqiang123 <1045612194@qq.com>
2020-06-08 20:50:46 +0800

    实现图片上传的改造，升级为fastFds

--- leyou-upload/src/main/java/com/leyou/service/impl/UploadServiceImpl.java
+++ leyou-upload/src/main/java/com/leyou/service/impl/UploadServiceImpl.java
@@ -1,18 +1,18 @@
 package com.leyou.service.impl;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.service.UploadService;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,8 +20,11 @@ import java.util.List;
 public class UploadServiceImpl implements UploadService {
     private static final Logger log = LoggerFactory.getLogger(UploadServiceImpl.class);
     private static final List<String> CONTENT_TYPES = Arrays.asList("image/png", "image/jpeg", "image/jpg");
-    private static final String filePath = "E:\\tools\\upload\\";
+    private static final String filePath = "E:\\code\\tools\\upload\\";
     private static final String url = "http://image.leyou.com/";
+
+    @Autowired
+    private FastFileStorageClient storageClient;
 
     @Override
     public String uploadImage(MultipartFile file) {
@@ -44,11 +47,14 @@ public class UploadServiceImpl implements UploadService {
             }
             // 2、保存图片
             // 2.1、保存到服务器
-            file.transferTo(new File(filePath + originalFilename));
-
+            // file.transferTo(new File(filePath + originalFilename));
+            String ext = StringUtils.substringAfterLast(originalFilename, ".");
+            StorePath storePath = this.storageClient
+                    .uploadImageAndCrtThumbImage(file.getInputStream(), file.getSize(), ext, null);
             // 2.3、返回url,进行回显
-            System.out.println(url + originalFilename);
-            return url + originalFilename;
+            System.out.println(url + storePath.getFullPath());
+            //  return url + originalFilename;
+            return url + storePath.getFullPath();
         } catch (IOException e) {
             log.error("服务器内部错误: {}" + originalFilename);
         }
